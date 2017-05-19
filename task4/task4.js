ed26bfe0796d390f832ff6c1cfe649914f244ae5
wangxiaoniu <13519870949@163.com>
2017-05-19 17:33:41 +0800

    tss4

--- task4/task4.js
+++ task4/task4.js
@@ -1,4 +1,4 @@
-
+/*案例：https://tobiaschan.github.io/BaiduJs/Btask4/Btask4.html*/
 
 //扩浏览器添加事件
 function addEvent(obj,type,fn){
@@ -9,25 +9,50 @@ function addEvent(obj,type,fn){
 	}
 }
 var btn =document.getElementById('submit');
+var box= document.getElementById('box');
+var text=document.getElementById('text');
+var defaultDeg=0;//初始角度
+var count=1;//初始化计数 
 
-btn.addEventListener('click',toChange,false);
+
 
 
 //小方块运动函数
-function toChange(){
-	var box= document.getElementById('box');
-	var value=document.getElementsByClassName('text')[0].value;
-	/*if(value=='TUN LEF'){
-		box.className='tun-left';
-	}else if(value=='TUN RIG'){
-		box.className='tun-right';
-	}*/
-	switch(value){
-		case 'TUN LEF':
-		box.className='tun-left';
-		break;
-		case 'TUN RIG':
-		box.className='tun-right';
+function toChange(e){
+	
+	if(e.value=='TUN LEF'){
+		if(count==1){
+			count=4;
+		}else{
+			count=(count-1)%4;
+		}
+		box.style.transform = "rotate("+(defaultDeg-90)+"deg)";
+		//getStyle(box,transform)="rotate("+(defaultDeg-90)+"deg)";
+		defaultDeg=-90+defaultDeg;//递增角度值
+		 console.log(count);
+	}
+	if(e.value=="TUN RIG"){
+        if(count==3) {
+        	count=4;
+        } else {
+        	count=(count+1)%4;
+        }
+        box.style.transform="rotate("+(defaultDeg+90)+"deg)";
+        defaultDeg=90+defaultDeg;
+        console.log(count);
+    }
+	if(e.value=='TUN BAC'){
+		if(count==2){
+			count=4;
+		}else{
+			count=(count+2)%4;
+		}
+		box.style.transform = "rotate("+(defaultDeg+180)+"deg)";
+		defaultDeg=180+defaultDeg;//递增角度值
+		 console.log(count);
 	}
 
 }
+addEvent(btn,'click',function(){
+	toChange(text);
+});
