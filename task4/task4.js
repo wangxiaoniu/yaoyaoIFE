7ed1130dd4742fd09ca4cbddde51028588f6a059
wangxiaoniu <13519870949@163.com>
2017-05-24 21:42:24 +0800

    525

--- task4/task4.js
+++ task4/task4.js
@@ -1,4 +1,8 @@
-/*案例：https://tobiaschan.github.io/BaiduJs/Btask4/Btask4.html*/
+/*案例：https://tobiaschan.github.io/BaiduJs/Btask4/Btask4.html
+
+参考资料：
+http://www.zhangxinxu.com/wordpress/2011/03/ie%E7%9F%A9%E9%98%B5%E6%BB%A4%E9%95%9Cmatrix%E4%B8%8B%E6%97%8B%E8%BD%AC%E4%B8%8E%E7%BC%A9%E6%94%BE%E5%8F%8A%E7%BB%93%E5%90%88transform%E7%9A%84%E6%8B%93%E5%B1%95/
+*/
 
 //扩浏览器添加事件
 function addEvent(obj,type,fn){
@@ -13,48 +17,37 @@ var box= document.getElementById('box');
 var text=document.getElementById('text');
 var defaultDeg=0;//初始角度
 var count=1;//初始化计数 
-
-//获取外链样式
-function getStyle(sname){
-	for(var i=0;i<document.styleSheets.length;i++){
-		var rules;
-		if(document.styleSheets[i].cssRules){//W3C
-
-			rules=document.styleSheets[i].cssRules;
-		}else{
-			rules=document.styleSheets[i].rules;
-		}
-		for(var j=0;j<rules.length;i++){
-			if(rules[j].selectorText==sname){
-				return rules[j].style;
-			}
-		}
-
-	}
+//查找外链样式
+function getstyle(sname) { 
+    for (var i=0;i< document.styleSheets.length;i++) { 
+        var rules; 
+        if (document.styleSheets[i].cssRules) { 
+            rules = document.styleSheets[i].cssRules; 
+        } else { 
+            rules = document.styleSheets[i].rules; 
+        } 
+        for (var j=0;j< rules.length;j++) { 
+            if (rules[j].selectorText == sname) { 
+                return rules[j].style; 
+            } 
+        } 
+    } 
 }
 
-
-function insertRule(sheet,selectorText,cssText,position){
-	//W3C
-	if(sheet.insertRule){
-		sheet.insertRule(selectorText+'{'+cssText+'}',position);
-	}else if(sheet.addRule){
-		sheet.addRule(selectorText,cssText,position);
-	}
-}
-//小方块运动函数
+//小方块旋转运动函数
 function toChange(e){
 	
 	if(e.value=='TUN LEF'){
 		if(count==1){
 			count=4;
+
 		}else{
 			count=(count-1)%4;
 		}
-		box.style.transform = "rotate("+(defaultDeg-90)+"deg)";
-		//getStyle(box,transform)="rotate("+(defaultDeg-90)+"deg)";
+		getstyle("#box").transform = "rotate("+(defaultDeg-90)+"deg)";
+		
 		defaultDeg=-90+defaultDeg;//递增角度值
-		 console.log(count);
+		 //console.log(count);
 	}
 	if(e.value=="TUN RIG"){
         if(count==3) {
@@ -64,7 +57,7 @@ function toChange(e){
         }
         box.style.transform="rotate("+(defaultDeg+90)+"deg)";
         defaultDeg=90+defaultDeg;
-        console.log(count);
+        //console.log(count);
     }
 	if(e.value=='TUN BAC'){
 		if(count==2){
@@ -74,42 +67,49 @@ function toChange(e){
 		}
 		box.style.transform = "rotate("+(defaultDeg+180)+"deg)";
 		defaultDeg=180+defaultDeg;//递增角度值
-		 console.log(count);
+		//console.log(count);
 	}
 	if(e.value=='GO'){
 		Go();
 	}
-
-
-
 }
+//判断移动方向及实现移动函数
 function Go(){
-	box.style.backgroundColor='yellow';
-	if(!box.style.left){
-		box.style.left="0px";
-	}
-	if(!box.style.top){
-		box.style.top="0px";
-	}
 	var final_x=500,final_y=500;
-	var xpos=parseInt(box.style.left);
-	var xpos=parseInt(box.style.top);
-	
-	if((final_x-xpos)>0){
-		if(count=1){
-			alert('1');
-		}
-		else if(count=2){
-			alert('2');
-		}
-		else if(count=3){
-			alert('3');
-		}
-		else if(count=4){
-			alert('4');
+	var xpos=parseInt(getstyle("#box").left);
+	var ypos=parseInt(getstyle("#box").top);
+	var dist=50;
+	if(count==1){
+		ypos=ypos-dist;
+		if(ypos>=0){
+			getstyle("#box").top=ypos+'px';
+			//console.log(ypos);
 		}
 	}
+	if(count==2){
+		xpos=xpos+dist;
+		if(xpos<=450){
+			getstyle("#box").left=xpos+'px';
+			//console.log(xpos);
+		}
+	}
+	if(count==3){
+		ypos=ypos+dist;
+		if(ypos<=(final_x-dist)){
+			getstyle("#box").top=ypos+'px';
+			//console.log(ypos);
+		}
+	}
+	else if(count==4){
+		xpos=xpos-dist;
+		if(xpos>=0){
+			getstyle("#box").left=xpos+'px';
+			//console.log(xpos);
+		}
+	}
+	
 }
+
 addEvent(btn,'click',function(){
 	toChange(text);
 });
